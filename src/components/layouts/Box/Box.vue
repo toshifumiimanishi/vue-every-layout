af66d10d20fb3a4bc2f1945594fcf35367d99782
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2022-05-29 08:26:45 +0900

    Add Box component

---
+++ src/components/layouts/Box/Box.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { withDefaults } from "vue";
+
+type BoxProps = {
+  backgroundColor?: string;
+  borderColor?: string;
+  borderRadius?: string;
+  borderWidth?: string;
+  padding: string;
+}
+
+const props = withDefaults(defineProps<BoxProps>(), {
+  borderColor: '#e2e8f0',
+  padding: '16px',
+})
+</script>
+
+<template>
+  <div class="box">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.box {
+  padding: v-bind('props.padding');
+  background-color: v-bind('props.backgroundColor');
+  border: v-bind('props.borderWidth') solid v-bind('props.borderColor');
+  border-radius: v-bind('props.borderRadius');
+}
+</style>
