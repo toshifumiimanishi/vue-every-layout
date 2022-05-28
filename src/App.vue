f91e350c3ed30d9c28999b25e2fd0aacea8e3dcd
Toshifumi Imanishi <13635766+toshifumiimanishi@users.noreply.github.com>
2022-05-29 08:31:46 +0900

    Init Vite

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { Center } from "./components/layouts/Center/";
+import { Sidebar } from "./components/layouts/Sidebar/";
+
+</script>
+
+<template>
+  <Center max="1200px">
+    <Sidebar space="3rem" side="right" content-min="75%" noStretch>
+      <template v-slot:sidebar>
+        <Center intrinsic>
+          <img alt="Vue logo" src="./assets/logo.png" />
+        </Center>
+      </template >
+      <template v-slot:not-sidebar>
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </template>
+    </Sidebar>
+  </Center>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { Box } from "./layouts/Box/";
+import { Center } from "./layouts/Center/";
+import { VStack, HStack } from "./layouts/Stack/";
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <VStack space="3rem" :split-after="2">
+    <Center max="640px" gutters="24px" andText intrinsic>
+      <h1>{{ msg }}</h1>
+      <Box borderWidth="1px" borderRadius="8px">
+        <p>
+          Recommended IDE setup:
+          <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
+          +
+          <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+        </p>
+
+        <p>See <code>README.md</code> for more information.</p>
+      </Box>
+      <HStack align="center" justify="center">
+        <p>
+          <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+          </a>
+          |
+          <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+        </p>
+        <button type="button" @click="count++">count is: {{ count }}</button>
+      </HStack>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+      </p>
+    </Center>
+  </VStack>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
